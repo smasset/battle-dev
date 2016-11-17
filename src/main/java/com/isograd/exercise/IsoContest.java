0821112231997f82e6d7f1f117d989c1d9d18e05
Sébastien Masset <smasset@server>
2016-11-17 18:42:54 +0100

    Solving first test

--- src/main/java/com/isograd/exercise/IsoContest.java
+++ src/main/java/com/isograd/exercise/IsoContest.java
@@ -7,11 +7,65 @@
  * ***/
 package com.isograd.exercise;
 
-import java.util.*;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class IsoContest {
+
+	private class Accumulator<T> extends HashMap<T, Integer> {
+		private static final long serialVersionUID = 5480917823114056491L;
+
+		public Accumulator(int initialSize) {
+			super(initialSize);
+		}
+
+		public void addCount(T key, Integer count) {
+			this.put(key, this.getCount(key) + count);
+		}
+
+		public void addCount(T key) {
+			this.addCount(key, 1);
+		}
+
+		public Integer getCount(T key) {
+			Integer currentCount = this.get(key);
+
+			if (currentCount == null) {
+				currentCount = 0;
+			}
+
+			return currentCount;
+		}
+	}
+
+	public String solveGants(InputStream input) {
+		Integer count = null;
+		String line = null;
+
+		Accumulator<String> gloves = null;
+		try (Scanner sc = new Scanner(input)) {
+			while (sc.hasNextLine()) {
+				line = sc.nextLine();
+				/* Lisez les données et effectuez votre traitement */
+				if (count == null) {
+					count = Integer.parseInt(line);
+					gloves = new Accumulator<>(count);
+				} else {
+					gloves.addCount(line);
+				}
+			}
+		}
+
+		/*
+		 * Vous pouvez aussi effectuer votre traitement une fois que vous avez
+		 * lu toutes les données.
+		 */
+		return gloves.values().stream().map(c -> Math.floor(c / 2d)).collect(Collectors.summingInt(Double::intValue))
+				.toString();
+	}
 
 	public String dummySolve(InputStream input) {
 		String line = null;
@@ -39,6 +93,6 @@ public class IsoContest {
 			input = System.in;
 		}
 
-		System.out.println(contest.dummySolve(input));
+		System.out.println(contest.solveGants(input));
 	}
 }
